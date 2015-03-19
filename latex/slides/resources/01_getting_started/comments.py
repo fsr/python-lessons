c8b10efa28fceb1f639d261afd94fb50462723c0
Felix Döring <Doering.Felix@googlemail.com>
2015-03-19 16:45:55 +0100

    make seperated codefiles

---
+++ latex/slides/resources/01_getting_started/comments.py
@@ -0,0 +1,9 @@
+# in python nur einzeilige Kommentare
+
+def my_function(params):
+    """
+    Oder docstrings wie dieser,
+    aber nur zu beginn einer Funktions-
+    oder Klassendefinition
+    """
+    pass
