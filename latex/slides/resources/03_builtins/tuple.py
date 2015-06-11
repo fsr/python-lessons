01457acf4ddb2176b04e05e0500b80c88f9b8360
justusadam <development@justusadam.com>
2015-06-11 13:30:09 +0200

    smaller font

--- latex/slides/resources/03_builtins/tuple.py
+++ latex/slides/resources/03_builtins/tuple.py
@@ -1,6 +1,5 @@
 # Tupel mit 3 Elementen
 t = (1, 3, 'ein string')
-# oder auch ohne klammern
 isinstance(t, tuple) # ==> True
 
 t[0] == 1  # ==> True
@@ -9,6 +8,7 @@ t[2] == 'ein string'  # ==> True
 t[4] == 'ein string'  # ==> IndexError: tuple index out of range
 t[2] = 'ein anderer string'  # ==> TypeError: 'tuple' object does not support item assignment
 
+# oder auch ohne klammern
 t = 1, 3, 'ein string'
 # macht es (manchmal) besser lesbar, z.b. bei
 return 1, 2, 5
