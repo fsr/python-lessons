d30d18d596a81c9270c45f1f32d606736e58b125
Felix Döring <Doering.Felix@googlemail.com>
2015-06-25 11:43:12 +0200

    add 06_decorators

---
+++ latex/slides/resources/06_decorators/decorator.py
@@ -0,0 +1,52 @@
+def get_date(date):
+    return ", today it's {}.".format(date)
+
+
+# unser decorator
+def tell_the_world(func):
+    def complete_sentence(date):
+        return "Hello World{}".format(func(date))
+    return complete_sentence
+
+# normaler Aufruf:
+print tell_the_world(get_date)("25th June 2015")
+
+# als decorator
+tell_the_world_its = tell_the_world(get_date)
+
+print tell_the_world_its("25th June 2015")
+# => Hello World, today it's 25th June 2015.
+
+
+
+# Nutzung des @ Syntaxes
+
+def tell_the_world(func):
+    def complete_sentence(date):
+        return "Hello World, {}".format(func(date))
+    return complete_sentence
+
+
+@tell_the_world
+def get_date(date):
+    return "today it's {}.".format(date)
+
+print get_date("25th June 2015")
+# => Hello World, today it's 25th June 2015.
+
+
+# Decorator mit Argumenten
+def tell_the_date_to(name):
+    def name_decorator(func):
+        def complete_sentence(date):
+            return "Hello {name}, {date}".format(name=name, date=func(date))
+        return complete_sentence
+    return name_decorator
+
+
+@tell_the_date_to("John Doe")
+def get_date(date):
+    return "today it's {}.".format(date)
+
+print get_date("25th June 2015")
+# => Hello John Doe, today it's 25th June 2015.
---
+++ latex/slides/resources/06_decorators/functions.py
@@ -0,0 +1,41 @@
+# Funktionen Variablen zuweisen
+def return_hello():
+    return "Hello"
+
+say_hello = return_hello
+
+print say_hello()
+# => Hello
+
+
+# Funktionen in Funktionen definieren
+def greet(name):
+    def return_hello():
+        return "Hello "
+    greeting = return_hello() + name
+    return greeting
+
+print greet("World")
+# => Hello World
+
+
+# Rueckgabe von Funktionen
+def greet():
+    def say_hello():
+        return "Hello"
+    return say_hello
+
+print greet()
+# Hello
+
+
+# Uebergabe von Funktionen
+def say_date(date):
+    return "Today it's {date}".format(date=date)
+
+def which_date(function)
+    date = "25th June 2015"
+    return function(date)
+
+print which_date(say_date)
+# => Today it's 25th June 2015
