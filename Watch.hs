a00fc84481c43027a70a035ef0b7473484835669
justusadam <development@justusadam.com>
2015-07-09 17:12:41 +0200

    added cmd argument to only compile

--- Watch.hs
+++ Watch.hs
@@ -4,13 +4,16 @@ import System.FSNotify
 import Control.Concurrent (threadDelay)
 import Control.Monad (forever)
 import Filesystem.Path.CurrentOS
+import Filesystem
 import System.Process
 import Prelude hiding (FilePath)
+import System.Environment
 
 
 sourceDir = "md/slides"
 targetHtmlDir = "rbuild"
 targetTexDir = "latex/slides"
+fileFilter = flip hasExtension "md"
 
 
 revealCommand infile outfile =
@@ -24,29 +27,42 @@ texCommand infile outfile =
 toTexName file = targetTexDir </> addExtension (basename file) "tex"
 
 
-build :: Event -> IO ()
-build e =
+buildEvent :: Event -> IO ()
+buildEvent e =
   case e of
-    (Added file _) -> action file
-    (Modified file _) -> action file
+    (Added file _) -> build file
+    (Modified file _) -> build file
     _ -> return ()
   where
-    action file = do
-      let revealName = toRevealName file
-      let texName = toTexName file
-      putStrLn $ "compiling file '" ++ encodeString file ++ "' to html file '" ++ encodeString revealName ++ "'"
-      callCommand $ revealCommand file revealName
-      putStrLn $ "compiling file '" ++ encodeString file ++ "' to tex file '" ++ encodeString texName ++ "'"
-      callCommand $ texCommand file texName
 
-main =
-  withManager $ \mgr -> do
-    -- start a watching job (in the background)
-    watchDir
-      mgr          -- manager
-      sourceDir          -- directory to watch
-      (flip hasExtension "md" . eventPath) -- predicate
-      build        -- action
 
-    -- sleep forever (until interrupted)
-    forever $ threadDelay maxBound
+build :: FilePath -> IO ()
+build file = do
+  let revealName = toRevealName file
+  let texName = toTexName file
+  putStrLn $ "compiling file '" ++ encodeString file ++ "' to html file '" ++ encodeString revealName ++ "'"
+  callCommand $ revealCommand file revealName
+  putStrLn $ "compiling file '" ++ encodeString file ++ "' to tex file '" ++ encodeString texName ++ "'"
+  callCommand $ texCommand file texName
+
+main = do
+  args <- getArgs
+  case args of
+    [] -> watch
+    ["watch"] -> watch
+    ["compile"] -> compile
+    [_] -> error "wrong command line arguments"
+    _ -> error "wrong number of arguments (expected 0 or 1)"
+  where
+    compile = do
+      allFiles <- listDirectory sourceDir
+      mapM_ build $ filter fileFilter allFiles
+    watch = do
+      compile
+      withManager $ \mgr -> do
+        watchDir
+          mgr
+          sourceDir
+          (fileFilter . eventPath)
+          buildEvent
+        forever $ threadDelay maxBound
