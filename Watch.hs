7dbf1d3ce67ce30810040c1cb2a0b54a02385b75
justusadam <development@justusadam.com>
2015-07-09 17:47:56 +0200

    made script prettier and shorter

--- Watch.hs
+++ Watch.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-} -- for FilePath literals
-
 import System.FSNotify
 import Control.Concurrent (threadDelay)
 import Control.Monad (forever)
@@ -15,36 +14,28 @@ sourceDir = "md/slides"
 targetHtmlDir = "rbuild"
 targetTexDir = "latex/slides"
 fileFilter = flip hasExtension "md"
-
-
-revealCommand =
-  printf "pandoc -t revealjs %s -o %s --no-highlight --template md/template.html -s"
-
+revealCommand = printf "pandoc -t revealjs %s -o %s --no-highlight --template md/template.html -s"
 toRevealName file = targetHtmlDir </> addExtension (basename file) "html"
-
-texCommand =
-  printf "pandoc -t beamer %v -o %v"
-
+texCommand = printf "pandoc -t beamer %s -o %s"
 toTexName file = targetTexDir </> addExtension (basename file) "tex"
 
 
-buildEvent :: Event -> IO ()
-buildEvent e =
+eventHandler :: Event -> IO ()
+eventHandler e =
   case e of
     (Added file _) -> build file
     (Modified file _) -> build file
     _ -> return ()
-  where
 
 
 build :: FilePath -> IO ()
 build file = do
-  putStrLn $ printf "compiling $ html '%v' -> '%v'" fileName revealName
+  putStrLn $ printf "compiling $ html '%s' -> '%s'" fileName revealName
   callCommand $ revealCommand fileName revealName
-  putStrLn $ printf "compiling $ tex '%v' -> '%v'" fileName texName
+  putStrLn $ printf "compiling $ tex '%s' -> '%s'" fileName texName
   callCommand $ texCommand fileName texName
   where
-    fileName  = encodeString file
+    fileName   = encodeString file
     revealName = encodeString $ toRevealName file
     texName    = encodeString $ toTexName file
 
@@ -56,8 +47,8 @@ main = do
     [] -> watch
     ["watch"] -> watch
     ["compile"] -> compile
-    [_] -> error "wrong command line arguments"
-    _ -> error "wrong number of arguments (expected 0 or 1)"
+    [_] -> error "unknown command line argument"
+    _ -> error "too many command line arguments (expected no more than one)"
   where
     compile = do
       allFiles <- listDirectory sourceDir
@@ -65,9 +56,5 @@ main = do
     watch = do
       compile
       withManager $ \mgr -> do
-        watchDir
-          mgr
-          sourceDir
-          (fileFilter . eventPath)
-          buildEvent
+        watchDir mgr sourceDir (fileFilter . eventPath) eventHandler
         forever $ threadDelay maxBound
