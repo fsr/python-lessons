1f75ff5f587102dd037ac0b47dbb4163173a526f
justusadam <development@justusadam.com>
2015-07-09 13:47:31 +0200

    Added tex build for md. Ran it.

--- reveal.js-build.sh
+++ reveal.js-build.sh
@@ -3,6 +3,8 @@ ALL=`ls md/slides/*.md`
 
 for path in $ALL
 do
-file=${path##*/}
+  file=${path##*/}
+
   pandoc -t revealjs -s --template md/template.html $path --no-highlight -o rbuild/${file%.md}.html
+  pandoc -t beamer -o latex/slides/${file%.md}.tex $path
 done
