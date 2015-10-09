5d9c5c4ce77c1788fd70d91447d4a12e216d5f55
h4llow3En <Doering.Felix@googlemail.com>
2015-10-09 17:17:26 +0200

    fixing reveal.js-install.sh script

--- reveal.js-install.sh
+++ reveal.js-install.sh
@@ -3,6 +3,6 @@ source_url=https://codeload.github.com/hakimel/reveal.js/tar.gz/3.1.0
 mkdir rbuild
 
 curl "$source_url" -o reveal.js.tar.gz
-
 tar -xf reveal.js.tar.gz
-
+mv reveal.js-3.1.0 rbuild/reveal.js
+rm reveal.js.tar.gz
