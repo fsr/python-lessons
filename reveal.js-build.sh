9b7d36790418beaae7035497e8e5fdc13c8b1a43
h4llow3En <Doering.Felix@googlemail.com>
2015-10-09 16:43:30 +0200

    comment out tex build of reveal.js This is because slides that aren't used for presentation will be handwritten again.

--- reveal.js-build.sh
+++ reveal.js-build.sh
@@ -6,5 +6,5 @@ do
   file=${path##*/}
 
   pandoc -t revealjs -s --template md/template.html $path --no-highlight -o rbuild/${file%.md}.html
-  pandoc -t beamer -o latex/slides/${file%.md}.tex $path
+# pandoc -t beamer -o latex/slides/${file%.md}.tex $path
 done
