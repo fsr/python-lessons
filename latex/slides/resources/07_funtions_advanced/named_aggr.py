6dea04e7a89f7cc90a622e25ab42be90aa6db010
h4llow3En <Doering.Felix@googlemail.com>
2015-11-23 16:11:08 +0100

    fix utf8

--- latex/slides/resources/07_funtions_advanced/named_aggr.py
+++ latex/slides/resources/07_funtions_advanced/named_aggr.py
@@ -6,4 +6,4 @@ function(1)    # korrekt, aggr = {}
 function(1, some=9)    # korrekt, agar = {'some': 9}
 function(some=6)    # inkorrekt, param1 braucht einen Wert
 function(some=0, param1=8, param2=4)
-# korrekt, agar = {'some': 0, ’param2': 4}
+# korrekt, agar = {'some': 0, 'param2': 4}
