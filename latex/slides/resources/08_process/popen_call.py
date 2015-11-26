96c5f9c958c84fe2fce3e3e9c94da6bc92877e32
Felix Wittwer <felix.wittwer@gmail.com>
2015-11-27 00:17:03 +0100

    Start rewriting process slides

---
+++ latex/slides/resources/08_process/popen_call.py
@@ -0,0 +1,7 @@
+import subprocess
+
+Popen(args, bufsize=-1, executable=None, stdin=None, stdout=None,
+      stderr=None, preexec_fn=None, close_fds=True, shell=False,
+      cwd=None, env=None, universal_newlines=False, startupinfo=None,
+      creationflags=0, restore_signals=True, start_new_session=False,
+      pass_fds=())
