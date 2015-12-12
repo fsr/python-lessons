3291730238d8b29f93bba7f6896ae02473254737
h4llow3En <Doering.Felix@googlemail.com>
2015-12-12 23:08:12 +0100

    finish tex of regex lesson

---
+++ latex/slides/resources/10_regex/match.py
@@ -0,0 +1,6 @@
+start([group])
+
+end([group])
+
+span([group])
+
---
+++ latex/slides/resources/10_regex/methods.py
@@ -0,0 +1,11 @@
+compile(pattern, flags=0)
+
+search(pattern, string, flags=0)
+
+match(pattern, string, flags=0)
+
+fullmatch(pattern, string, flags=0)
+
+findall(pattern, string, flags=0)
+
+finditer(pattern, string, flags=0)
