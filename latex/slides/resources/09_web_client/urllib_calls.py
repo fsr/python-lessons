7ae855041a3bbf9b3042bf2e51d7b0ce4b15bac5
Felix Wittwer <dev@felixwittwer.de>
2016-05-31 17:59:55 +0200

    Change slide 09 to new version

--- latex/slides/resources/09_web_client/urllib_calls.py
+++ latex/slides/resources/09_web_client/urllib_calls.py
@@ -1,13 +1,13 @@
 import urllib.request
 
 urllib.request.urlopen(url, data=None, [timeout, ] *,
-                       cafile=None, capath=None, cadefault=False,
-                       context=None)
+                       cafile=None, capath=None, 
+                       cadefault=False, context=None)
 
 
 urllib.request.Request(url, data=None, headers={},
-                       origin_req_host=None, unverifiable=False,
-                       method=None)
+                       origin_req_host=None,
+                       unverifiable=False, method=None)
 
 
 http.client.HTTPResponse(sock, debuglevel=0,
