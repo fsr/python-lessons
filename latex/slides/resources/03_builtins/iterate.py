4b7e0833f59bc8420cd4e84f741f7e6dadc07848
Eric Wolf <robo-eric@gmx.de>
2021-10-16 02:00:05 +0200

    rework iteration
    This should address issue #8 by moving iteration behind unpacking to
    prevent confusion with for loop examples.
    Also, the slide has been separated in iterators and iterables and
    contains information about __next__, StopIteration, all, any and caveats.
    Furthermore the examples have been reworked to contain no long strings
    and show more attributes of iterators.

--- latex/slides/resources/03_builtins/iterate.py
+++ latex/slides/resources/03_builtins/iterate.py
@@ -3,34 +3,34 @@ for i in [1,2,3]:
         break
         # verlässt die Schleife
 else:
-    pass
     # wenn kein break vorkommt
+    print("keine Zahl größer als 9")
 
-for i in (1,2,3):
-    # code
-    pass
+# Iterator über die Zahlen von 0 bis 3 (3 nicht enthalten)
+iterator = iter(range(0, 3))
 
-for i in {1:'value1', 2:'value2'}:
-    # iteration ueber die keys
-    pass
+# gibt 0, 1 und 2 aus
+for i in iterator:
+    print(i)
 
+# kein Output, Iterator ist erschöpft
+for i in iterator:
+    print(i)
 
+# gibt 1, 'value1' und 2, 'value2' aus
 for i in {1:'value1', 2:'value2'}.items():
-    # i ist tuple von (key, value)
-    pass
+    # i ist ein Tuple von (key, value)
+    print(i[0], i[1])
 
-for value1, value2 in [
-        (1, 'werner'),
-        (3, 'geh mal in den keller'),
-        (42, 'ich glaub die russen komm\'')
-    ]:
-    # iteration mit tuple unpacking
-    # code
-
-# oder auch
-
-for value1, value2 in zip([1,3,42], ['werner', 'geh mal in den keller', 'ich glaub die russen komm\'', 'dieser string wird in der iteration nicht auftauchen', 'dieser auch nicht'])
-
+# iteration mit tuple unpacking
+# gleiche Ausgabe wie letztes Beispiel
 for key, value in {1:'value1', 2:'value2'}.items():
-    # iteration ueber keys und values mit tuple unpacking
-    pass
+    print(key, value)
+
+# zip verbindet zwei Iterables und stoppt wenn einer die Elemente ausgehen
+# -4 wird deswegen nicht ausgegeben
+for value1, value2 in zip((1,2,3), (-1,-2,-3,-4)):
+    print(value1, value2)
+
+# True != False
+any((False, True, False)) != all((False, True, False))
